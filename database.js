a7b79c422630d6b3d45bd31c50bf2076e869eabf
gowriwagge <gowriwagge@gmail.com>
2024-09-16 11:05:24 +0530

    added database

---
+++ database.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'Gowriwagge',   // Replace with your MySQL user
+  password: 'Gowri08', // Replace with your MySQL password
+  database: 'SheettoDb', // Replace with your database name
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+    return;
+  }
+  console.log('Connected to the database.');
+});
+
+module.exports = connection;
