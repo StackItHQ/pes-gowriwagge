36efacf453753e865c18841c095c0f677e5813a3
gowriwagge <gowriwagge@gmail.com>
2024-09-16 11:20:38 +0530

    added token

---
+++ token.js
@@ -0,0 +1,5 @@
+{"access_token":"ya29.a0AcM612xr8qAnraQrgNNu8AVVuHu1s6WWCvZ1UhCpXGZA2txs5UMKQLtd5iRP_qOeot9SCAIn__pRrj-vK3g9BavNVAc6O0a-Z9g260AAfddzsvWpkOnYLOKz7SFG6N1efz0A_xq_5ynwQ9vIrnDDm8P1p2bsOu6oHS2kHFERaCgYKAbUSARISFQHGX2Mi2v-aiBw4S2_sKtzvl0SV0A0175",
+    "refresh_token":"1//0glyXk-NejqceCgYIARAAGBASNwF-L9IrZXgPkDsFXbZZ3r7nnQMFLW7jOpiq8ho5DOY-S27-pIx7AS2SGcWb99kM0O3uOoVCwUA",
+    "scope":"https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/drive",
+    "token_type":"Bearer",
+    "expiry_date":1726402639680}
